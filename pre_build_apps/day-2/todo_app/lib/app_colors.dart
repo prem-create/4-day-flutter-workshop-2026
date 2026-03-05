6afde98f7d446b4fe60eac3b45244d124e23c728
prem-create <premdilliwar31@gmail.com>
2026-03-05 18:24:24 +0530

    homepage complete

---
+++ pre_build_apps/day-2/todo_app/lib/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+//constant colors through out the app
+const Color primaryColor= Color(0xFF0AB6AB);
+const Color secondaryColor= Color(0xFF151515);
+const Color textPrimaryColor= Colors.white;
+const Color textSecondaryColor= Colors.white70;
