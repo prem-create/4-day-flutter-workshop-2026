c9fbf7136ea879112bbae3bb20a63d2c37c87643
prem-create <premdilliwar31@gmail.com>
2026-03-05 16:26:08 +0530

    counter app ui

---
+++ pre_build_apps/day-2/counter_app/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Home());
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  int tapCount = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter App')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      body: Center(
+        child: Text('no of time you have tapped: ' + tapCount.toString()),
+      ),
+    );
+  }
+}
